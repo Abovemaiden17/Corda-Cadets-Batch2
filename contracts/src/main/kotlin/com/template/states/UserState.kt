cabbb9dc92ae603a49b405227e321f40975cdd34
lkjh3111 <john@quantumcrowd.com>
2019-07-25 08:52:37 +0800

    added state and contract

---
+++ contracts/src/main/kotlin/com/template/states/UserState.kt
@@ -0,0 +1,16 @@
+package com.template.states
+
+import com.template.contracts.UserContract
+import net.corda.core.contracts.BelongsToContract
+import net.corda.core.contracts.LinearState
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.Party
+
+@BelongsToContract(UserContract::class)
+data class UserState(val email: String,
+                     val username: String,
+                     val password: String,
+                     val user: Party,
+                     override val linearId: UniqueIdentifier = UniqueIdentifier(),
+                     override val participants: List<AbstractParty>): LinearState
