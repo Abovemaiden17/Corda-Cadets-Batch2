e1259d88036b36b664eb29ff8c4191bce7272434
jd0798 <jhan@quantumcrowd.io>
2019-07-25 16:55:14 +0800

    added UserFlowsUpdate

--- contracts/src/main/kotlin/com/template/contracts/UserContract.kt
+++ contracts/src/main/kotlin/com/template/contracts/UserContract.kt
@@ -13,6 +13,7 @@ class UserContract : Contract {
     interface Commands : CommandData {
         class Register : TypeOnlyCommandData(),Commands
         class Validate : TypeOnlyCommandData(),Commands
+        class Update : TypeOnlyCommandData(),Commands
     }
 
     override fun verify(tx: LedgerTransaction) {
@@ -28,6 +29,11 @@ class UserContract : Contract {
             }
 
             is Commands.Validate -> requireThat {
+
+
+            }
+
+            is Commands.Update -> requireThat {
 
 
             }
